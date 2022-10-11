45a5af58c96e623a96bab99a96c73d3d8acff92d
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-11 13:35:53 -0300

    Criado funções para atender requisito 16, tentando descobrir qual o erro

--- script.js
+++ script.js
@@ -7,28 +7,78 @@ function getWords() {
 
   return arrayWords;
 }
-
-function clearP() {
+const getElementP = document.getElementById('carta-gerada');
+function clear() {
   const getSpanElements = document.querySelectorAll('.span-word');
   for (let index = 0; index < getSpanElements.length; index += 1) {
     getSpanElements[index].remove();
   }
+  getElementP.innerText = '';
+}
+
+function randomStyle() {
+  const randomNumber = Math.round(Math.random() * 9) + 1;
+  let wordStyle = '';
+  switch (randomNumber) {
+  case 1:
+    wordStyle = 'newspaper';
+    break;
+  case 2:
+    wordStyle = 'magazine1';
+    break;
+  case 3:
+    wordStyle = 'magazine2';
+    break;
+  case 4:
+    wordStyle = 'medium';
+    break;
+  case 5:
+    wordStyle = 'big';
+    break;
+  case 6:
+    wordStyle = 'reallybig';
+    break;
+  case 7:
+    wordStyle = 'rotateleft';
+    break;
+  case 8:
+    wordStyle = 'rotateright';
+    break;
+  case 9:
+    wordStyle = 'skewleft';
+    break;
+  case 10:
+    wordStyle = 'skewright';
+    break;
+  default:
+    break;
+  }
+  return wordStyle;
+}
+
+function bothStyles(style1) {
+  let style2 = style1;
+  for (let index = 0; style2 === style1; index += 1) {
+    style2 = randomStyle();
+  }
+  return style2;
 }
 
 function createLetter() {
   const getInputContent = document.getElementById('carta-texto').value;
-  const getElementP = document.getElementById('carta-gerada');
   const arrayWords = getWords();
   if (getInputContent === '' || arrayWords[0] === '') {
     getElementP.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    clearP();
-    getElementP.innerText = '';
+    clear();
     for (let index = 0; index < arrayWords.length; index += 1) {
       const span = document.createElement('span');
       const getPElement = document.getElementById('carta-gerada');
       span.innerText = arrayWords[index];
-      span.className = 'span-word';
+      span.classList.add('span-word');
+      const style1 = randomStyle();
+      span.classList.add(style1);
+      span.classList.add(bothStyles(style1));
       getPElement.appendChild(span);
     }
   }
